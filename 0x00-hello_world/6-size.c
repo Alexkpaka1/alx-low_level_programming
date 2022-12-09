aa4058fbc79d130fb7598459b2c5047053227cec
Tinashe <117823479+tinashelorenzi@users.noreply.github.com>
2022-12-10 01:25:51 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -19,6 +19,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(day));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(dancer));
 	printf("Size of a float: %lu byte(s)\n", sizeof(gym));
-	
+
 	return (0);
 }
