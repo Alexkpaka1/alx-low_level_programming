893ddbc7f04ae03850f8ea49c2fd364f2a1220d6
alexkpaka1 <alexkpaka6@gmail.com>
2022-12-12 20:34:14 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+ *main - Entry point
+ *Return: Always 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		printf("%d", n);
+	}
+	putchar('\n');
+	return (0);
+}
