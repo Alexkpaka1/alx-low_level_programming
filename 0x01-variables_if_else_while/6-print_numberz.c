66668ce3bcf3ada24e1d1c0a3e3b675da753185d
alexkpaka1 <alexkpaka6@gmail.com>
2022-12-12 20:41:44 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+/**
+ *main - ntry point
+ *Return: Always
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+
+	for (a = '1' ; a <= '9' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
