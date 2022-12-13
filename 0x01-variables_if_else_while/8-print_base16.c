9a3d52b7e0740306f89edfacca28b4d6470cd9a1
alexkpaka1 <alexkpaka6@gmail.com>
2022-12-12 21:43:09 -0800

    task 8 print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+/**
+ * main -A program that prints unmber of the base 16 in lowercase
+ * Return: Always return 0 SUCCESSFUL MGE
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	char b;
+
+
+	for (a = '0' ; a ; <= '9' ; a++)
+	{
+		putchart(a);
+	}
+	for (b = 'a' ; b <= 'f' ; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
