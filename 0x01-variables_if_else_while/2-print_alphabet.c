c6c55fa62bbb8f795e3260669b04ec2aaba4d790
alexkpaka1 <alexkpaka6@gmail.com>
2022-12-12 19:53:10 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+/**
+ * main - Entry point
+ * Return: AAlways 0
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	putchart('\n');
+	return (0);
+}
