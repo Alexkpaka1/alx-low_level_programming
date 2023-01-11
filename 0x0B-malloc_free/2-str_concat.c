9e13abd97ef43ef1b485d4608501a502db5c6b7d
alexkpaka1 <alexkpaka6@gmail.com>
2023-01-11 12:06:29 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
-		lent1++;
+		len1++;
 	while (s2 && s2[len2])
 		len2++;
 
@@ -22,8 +22,9 @@ char *str_concat(char *s1, char *s2)
 	if (s3 == NULL)
 		return (NULL);
 
+
 	i = 0;
-	j == 0;
+	j = 0;
 
 	if (s1)
 	{
@@ -47,5 +48,3 @@ char *str_concat(char *s1, char *s2)
 
 	return (s3);
 }
-
-
